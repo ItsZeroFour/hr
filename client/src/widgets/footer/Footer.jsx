84d0c997c531ae864ebc48d2b7cff5372eb1b367
itsZeroFour <itsZeroFour@gmail.com>
2024-11-26 14:04:05 +0300

    add some info

--- client/src/widgets/footer/Footer.jsx
+++ client/src/widgets/footer/Footer.jsx
@@ -19,7 +19,9 @@ const Footer = () => {
               <Link href="mailto:salespro.connectors@mail.ru">
                 salespro.connectors@mail.ru
               </Link>
-              <Link href="tel:+70988534313">+7(098) 853-43-13</Link>
+              <Link href="tel:+70988534313">
+                Telegram/WhatsApp: +7(098) 853-43-13
+              </Link>
             </div>
 
             <div className={style.footer__center}>
@@ -32,14 +34,27 @@ const Footer = () => {
               <p>Мы в соц. сетях</p>
               <ul>
                 <li>
-                 <Link href="#"><Vk /></Link>
+                  <Link href="#">
+                    <Vk />
+                  </Link>
                 </li>
 
                 <li>
-                  <Link href="#"><Telegram /></Link>
+                  <Link href="#">
+                    <Telegram />
+                  </Link>
                 </li>
               </ul>
               <Link href="/authors">Авторы</Link>
+              <div className={style.footer__IP}>
+                <p>ИП Левин Лев Викторович </p>
+                <p>ОГРНИП: 324619600235201 ИНН: 616805092583</p>
+                <p>
+                  Адрес регистрации: 344019,,, Ростов-на-Дону г,, Мясникова ул,
+                  д 52/32{" "}
+                </p>
+                <p>Номер телефона +7 (968) 997-21-94</p>
+              </div>
               <Link href="https://uwustudio.ru/" style={{ display: "none" }}>
                 UWUSTUDIO
               </Link>
